48750458110f0bf7f1375bdd43b785116e2b30d4
Bilal TAS <bilaltas@me.com>
2021-05-02 11:48:01 +0300

    Button component created

---
+++ components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+	<a :href="href" class="button"><slot>BUTTON</slot></a>
+</template>
+
+<script>
+export default {
+	props: {
+		href: {
+			default: "#"
+		}
+	}
+};
+</script>
+
+<style lang="postcss">
+.button {
+	@apply text-white
+	bg-cc-main
+	font-bold
+	text-sm
+	px-6
+	py-3
+	rounded-full
+	mr-1
+	mb-1
+	inline-flex
+	items-center
+	hover:bg-white
+	hover:text-cc-main;
+}
+</style>
