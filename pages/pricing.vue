e3f87fc3e2a66fa34c2c7736e043d79cdf4edd09
Bilal TAS <bilaltas@me.com>
2021-09-21 09:53:29 +0300

    Plan features update

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -79,19 +79,19 @@
 									<svg width="12" height="8" fill="none" class="mr-3 text-teal-400">
 										<path d="M1.5 4.5l2.236 2.236a1 1 0 001.467-.056L10.5.5" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"></path>
 									</svg>
-									License for 1 Website
+									License for Unlimited Websites
 								</dd>
 								<dd class="flex items-center">
 									<svg width="12" height="8" fill="none" class="mr-3 text-teal-400">
 										<path d="M1.5 4.5l2.236 2.236a1 1 0 001.467-.056L10.5.5" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"></path>
 									</svg>
-									Support for 1 Website
+									Support for Unlimited Websites
 								</dd>
 								<dd class="flex items-center">
 									<svg width="12" height="8" fill="none" class="mr-3 text-teal-400">
 										<path d="M1.5 4.5l2.236 2.236a1 1 0 001.467-.056L10.5.5" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"></path>
 									</svg>
-									Updates for 1 Website
+									Updates for Unlimited Websites
 								</dd>
 								<dd class="flex items-center">
 									<svg width="12" height="8" fill="none" class="mr-3 text-teal-400">
